23b1a75f878b9c4cbea12c86b597267777740f0c
binghsj <binghsj@binghsj.com>
2022-02-24 20:06:40 +0800

    idea hot-fix first commit

--- module_git/src/package_git/ClassGit.java
+++ module_git/src/package_git/ClassGit.java
@@ -4,5 +4,6 @@ public class ClassGit {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello.git1");
+        System.out.println("hot-fix modify");
     }
 }
