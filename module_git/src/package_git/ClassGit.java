5b911f084e7294ce7a16f495e592cfa03e0cbd8f
binghsj <binghsj@binghsj.com>
2022-02-24 20:11:29 +0800

    idea hot-fix second commit

--- module_git/src/package_git/ClassGit.java
+++ module_git/src/package_git/ClassGit.java
@@ -5,5 +5,6 @@ public class ClassGit {
         System.out.println("hello,git");
         System.out.println("hello.git1");
         System.out.println("hot-fix modify");
+        System.out.println("hot-fix modify-1");
     }
 }
